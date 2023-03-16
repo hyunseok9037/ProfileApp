a2d3523acaa413fa1a665192d8e813eb710ed15b
hyunseok <dopocoo@naver.com>
2023-03-16 09:12:01 +0900

    profile 버튼까지만듬

---
+++ lib/theme.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+ThemeData theme(){
+  return ThemeData(
+    appBarTheme: AppBarTheme(
+      backgroundColor: Colors.white,
+      foregroundColor: Colors.black,
+      iconTheme: IconThemeData(
+        color: Colors.blue
+      )
+    ),
+  );
+}
